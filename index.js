3683216e05424088c47e2e2683edda7dfc867809
Your Name <you@example.com>
2022-09-05 16:51:43 +0400

    Telebot version 1

---
+++ index.js
@@ -0,0 +1,58 @@
+import TelegramBot from "node-telegram-bot-api";
+import Censor from "clean-armenian"
+import express from "express"
+let LastMessage = [];
+let Output = 0;
+const Token = ""
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+const bot = new TelegramBot(Token, { polling: true });
+
+
+let fix = function(val){
+  console.log (val)
+  Output = Censor(val)
+  return Output
+  }
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  LastMessage[0] = msg.text.toString()
+  fix(LastMessage[0])
+  bot.sendMessage(chatId, '👍');
+  app.post('/send',(req,res)=>{
+    res.redirect('/')
+    
+  })
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send(`
+  <head><link rel="shortcut icon" href="#"></head>
+     <h1>Enter your text<h1> 
+     <form action = "/send" method="POST">
+     <input type = "text" name ="mes">
+     <button>Submit</button>
+     </form>
+     <p>${Output}<p>
+     `
+     )
+     
+})
+
+
+
+app.post('/send',(req,res)=>{
+  LastMessage[0] = req.body.mes.toString()
+  fix(LastMessage[0])
+  res.redirect('/')
+  
+})
+
+LastMessage[0] = 0;
+
+app.listen(3000, () => {
+  console.log(`Running`)
+})
